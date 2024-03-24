07c9ac66fc87a28b763463f74cb2c7b868d71784
sjimeneh <samuelhz1998@gmail.com>
2024-03-24 15:32:35 -0500

    Se estructura la clase Transaction con sus atributos y relacioens. También se añade el Enumerador TransactionType

--- src/main/java/com/prueba/entidad/financiera/entity/Transaction.java
+++ src/main/java/com/prueba/entidad/financiera/entity/Transaction.java
@@ -1,4 +1,47 @@
 package com.prueba.entidad.financiera.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.prueba.entidad.financiera.entity.enums.TransactionType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+
+    @NotNull
+    private BigDecimal amount;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime dateTransaction;
+
+    @NotNull
+    @ManyToOne
+    @JsonIgnoreProperties(value = "transactions")
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @NotNull
+    @ManyToOne
+    @JsonIgnoreProperties(value = "transactions")
+    @JoinColumn(name = "destination_account_id")
+    private Product destinationProduct;
+
 }
---
+++ src/main/java/com/prueba/entidad/financiera/entity/enums/TransactionType.java
@@ -0,0 +1,7 @@
+package com.prueba.entidad.financiera.entity.enums;
+
+public enum TransactionType {
+    DEPOSIT, // Consignación
+    WITHDRAWAL, // Retiro
+    TRANSFER // Transferencia entre cuentas
+}
